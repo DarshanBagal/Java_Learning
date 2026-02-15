ca45e3b43695a3505765032dfc18f950d4f5893b
DarshanBagal <darshanbagal200411@gmail.com>
2026-02-16 00:04:20 +0530

    iam working new program today

---
+++ src/DataTypesDemo.java
@@ -0,0 +1,27 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 200;
+        int age = 21;
+        long population = 1400000000L;
+        float price = 99.99f;
+        double percentage = 67.5;
+
+        char grade = 'A';
+        boolean isPassed = true;
+        System.out.println("Byte value: " + b);
+        System.out.println("short value: " + s);
+        System.out.println("int value: " + age);
+        System.out.println("long value: " + population);
+        System.out.println("float value: " + price);
+        System.out.println("double value: " + percentage);
+
+
+
+
+
+
+
+    }
+}
+
