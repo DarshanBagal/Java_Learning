0ce2440d045bd1b79550dd0feff03f2e9341ceb9
DarshanBagal <darshanbagal200411@gmail.com>
2026-02-15 22:38:21 +0530

    iam working new program today

---
+++ src/student.java
@@ -0,0 +1,17 @@
+public class student {
+    public static void main(String[] args) {
+        String name = "darshan";
+        int age =21;
+        double percentage = 67;
+        boolean isPassed =true;
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+        if (percentage>=30){
+            System.out.println("Passed");
+
+        }
+        else {
+            System.out.println("Fail");
+        }
+
+    }
+}
