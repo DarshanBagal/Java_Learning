a83d3e90a51cc493bbe4e353aeb439c36a760435
DarshanBagal <darshanbagal200411@gmail.com>
2026-02-21 12:55:28 +0530

    iam working new program today

--- src/first.java
+++ src/first.java
@@ -3,6 +3,11 @@ import java.util.*;
 
 public class first {
     public static void main(String args[]){
+       String name;
+       Scanner sc=new Scanner(System.in);
+       System.out.println("my I know your name");
+       name=sc.nextLine();
+       System.out.println("Wellcome "+name);
 
     }
 }
