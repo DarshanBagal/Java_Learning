9cc9d9c0839faedcf84e4edcf20303b3efe1a5eb
DarshanBagal <darshanbagal200411@gmail.com>
2026-03-05 22:48:43 +0530

    iam working new program today

--- src/datasizerange.java
+++ src/datasizerange.java
@@ -1,8 +1,9 @@
+import jdk.dynalink.beans.StaticClass;
+
 import java.lang.*;
 public class datasizerange {
     public static void main(String[] args){
-        System.out.println("int min :"+Integer.MIN_VALUE);
-        System.out.println("int max :"+Integer.MAX_VALUE);
-        System.out.println("int byte :"+Integer.BYTES);
+        System.out.println("int min:"+Integer.MIN_VALUE);
+        System.out.println("int max:"+Integer.MAX_VALUE);
     }
 }
