778d2adac96f663528b81953bbeb94d6f1088630
DarshanBagal <darshanbagal200411@gmail.com>
2026-03-05 23:03:50 +0530

    iam working new program today

--- src/datasizerange.java
+++ src/datasizerange.java
@@ -6,6 +6,12 @@ public class datasizerange {
         System.out.println("int min:"+Integer.MIN_VALUE);
         System.out.println("int max:"+Integer.MAX_VALUE);
         System.out.println("int byte:"+Integer.BYTES);
-        
+        System.out.println("int Bytemax:"+Byte.MAX_VALUE);
+        System.out.println("int Bytemin:"+Byte.MIN_VALUE);
+        System.out.println("int Shortmin:"+Short.MIN_VALUE);
+        System.out.println("int ShortMax:"+Short.MAX_VALUE);
+
+
+
     }
 }
