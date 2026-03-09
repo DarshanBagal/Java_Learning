9b2325932080beb4c75001b4d3c82c7d3f049740
DarshanBagal <darshanbagal200411@gmail.com>
2026-03-09 21:39:17 +0530

    iam working new program today

---
+++ src/datatypesRange.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+public class datatypesRange {
+    public static void main(String[] args){
+        System.out.println("int min:"+Integer.MIN_VALUE);
+        System.out.println("int max:"+Integer.MAX_VALUE);
+        System.out.println("Byte min:"+Byte.MIN_VALUE);
+        System.out.println("Byte max:"+Byte.MAX_VALUE);
+        System.out.println("Short min:"+Short.MIN_VALUE);
+        System.out.println("Short max:"+Short.MAX_VALUE);
+        System.out.println("Byte value:"+Integer.BYTES);
+        System.out.println("Size value:"+Integer.SIZE);
+    }
+}
