160d65c050d9d33d5918dec98a5af575f9fb0fc4
DarshanBagal <darshanbagal200411@gmail.com>
2026-03-12 23:14:29 +0530

    iam working new program today

---
+++ src/testscan.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class testscan {
+    public static void main(String[] args){
+        Scanner scanner =new Scanner(System.in);
+        String myString = scanner.next();
+
+        System.out.println("My string:"+myString);
+    }
+}
