1b1af84f262f4561d56f618a4b248060dee3ea6e
DarshanBagal <darshanbagal200411@gmail.com>
2026-03-13 20:08:01 +0530

    iam working new program today

---
+++ src/Ifelsetest.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class Ifelsetest {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if (n % 2 == 0){
+            System.out.println("this Number is even");
+        }
+        else {
+            System.out.println("this Number is odd");
+        }
+
+    }
+}
