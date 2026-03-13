be3c8eed62e6a6dbd9365821c98531b4ae46e539
DarshanBagal <darshanbagal200411@gmail.com>
2026-03-13 20:14:56 +0530

    iam working new program today

--- src/Ifelsetest.java
+++ src/Ifelsetest.java
@@ -5,12 +5,13 @@ public class Ifelsetest {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        if (n % 2 == 0){
-            System.out.println("this Number is even");
-        }
-        else {
-            System.out.println("this Number is odd");
+        while (true) {
+            int n = scanner.nextInt();
+            if (n % 2 == 0) {
+                System.out.println("this Number is even");
+            } else {
+                System.out.println("this Number is odd");
+            }
         }
 
     }
