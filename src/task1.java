35c8a6ec2fe7d6b76654bd4a62a57345106fd6fa
DarshanBagal <darshanbagal200411@gmail.com>
2026-03-13 20:47:17 +0530

    iam working new program today

---
+++ src/task1.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int i = scanner.nextInt();
+        double d = scanner.nextDouble();
+        scanner.nextLine();
+        String s = scanner.nextLine();
+
+        System.out.println("String:"+s);
+        System.out.println("Double:"+d);
+        System.out.println("Integer:"+i);
+    }
+}
