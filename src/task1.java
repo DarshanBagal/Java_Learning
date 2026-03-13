b214841a525c80f5ea5ca449a504883023049b86
DarshanBagal <darshanbagal200411@gmail.com>
2026-03-13 21:06:33 +0530

    iam working new program today

--- src/task1.java
+++ src/task1.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 public class task1 {
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the Integer values");
         int i = scanner.nextInt();
+        System.out.println("Enter the Double values");
         double d = scanner.nextDouble();
         scanner.nextLine();
+        System.out.println("Enter the String values");
         String s = scanner.nextLine();
 
         System.out.println("String:"+s);
